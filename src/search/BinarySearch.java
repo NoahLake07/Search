2d3f2869d11dec5d8916c9c22641699a7a89c789
Noah Lake (School) <noahlake07@gmail.com>
2023-01-09 09:04:42 -0700

    Initial Commit

---
+++ src/search/BinarySearch.java
@@ -0,0 +1,80 @@
+package search;
+
+public class BinarySearch {
+
+    /**
+     * Performs a binary search on an int[] and searches for a given int target. The method returns
+     * the index the target is at, but if the target is not found, the method will return '-1'.
+     * @param arr int array to search
+     * @param target int target to search for
+     * @return the index the target is at in the given int array (arr)
+     */
+    public static int iterativeBinarySearch(int[] arr, int target){
+        // instantiate split pointer values
+        int leftPointer = 0;
+        int rightPointer = arr.length-1;
+
+        // while the pointers are not crossed, keep searching
+        while(leftPointer <= rightPointer) {
+            // find the middle index
+            int mid = (leftPointer + leftPointer)/2;
+
+            // check if the middle value matches target
+            if(arr[mid] == target){
+                return mid;
+            }
+
+            // move the pointers
+            if(arr[mid] > target){
+                // left half
+                rightPointer = mid - 1;
+            }
+            if(arr[mid] < target){
+                // right half
+                leftPointer = mid + 1;
+            }
+
+        }
+        // if this statement is reached, the target data was not found.
+        return -1;
+    }
+
+    /**
+     * A recursive method that finds the target value of an int array
+     * @param arr
+     * @param target
+     * @param left
+     * @param right
+     * @return the index the target is at, or the recursive method with new bounds
+     */
+    public static int recursiveBinarySearch(int[] arr, int target, int left, int right){
+        // find the middle index
+        int mid = (left + left)/2;
+
+        // base cases
+        if(left >= right){
+            return -1;
+        }
+        if(arr[mid] == target){
+            return mid;
+        }
+
+        // move the pointers
+        if(arr[mid] > target){
+            // left half
+            return recursiveBinarySearch(arr, target, mid, right - 1);
+        }
+        if(arr[mid] < target){
+            // right half
+            return recursiveBinarySearch(arr, target, mid + 1, right);
+        }
+
+        // if this statement is reached, then the target data was not found.
+        return -1;
+    }
+    public static int recursiveBinarySearch(int[] arr, int target){
+        return recursiveBinarySearch(arr, target, arr[0], arr.length-1);
+    }
+
+
+}
---
+++ src/search/LinearSearch.java
@@ -0,0 +1,63 @@
+package search;
+
+public class LinearSearch<T> {
+
+    /**
+     * Searches a string array and returns the index of the target found
+     * @param data
+     * @param target
+     * @return the index of target found
+     */
+    public int search(T[] data, T target){
+        for (int i = 0; i < data.length-1; i++) {
+            if(data[i].equals(target)){
+                System.out.println("\t * FOUND '" + target + "' AT INDEX " + i);
+                return i;
+            }
+        }
+        System.out.println("\t > TARGET NOT FOUND!");
+        return -1;
+    }
+
+    /**
+     * Searches a double array and returns the index of the target found
+     * @param data
+     * @param target
+     * @return the index of target found
+     */
+    public int search(double[] data, double target){
+        for (int i = 0; i < data.length-1; i++) {
+            if(data[i] == target){
+                System.out.println("\t * FOUND '" + target + "' AT INDEX " + i+1);
+                return i+1;
+            }
+        }
+        System.out.println("\t > TARGET NOT FOUND!");
+        return -1;
+    }
+
+    public static void testSearch(){
+        LinearSearch x = new LinearSearch();
+
+        String[] studentNames = {"Alice", "Bob", "Charlie", "David", "Eve"};
+        String target = "Charlie";
+        System.out.println(x.search(studentNames, target));
+
+        double[] sheepWeights = {100.0, 105.5, 110.0, 115.0, 120.5};
+        double target2 = 111.0;
+        System.out.println(x.search(sheepWeights, target2));
+
+        String[] DNA = {"ATCG", "GTAC", "CGAT", "TCGA"};
+        String target3 = "CGAT";
+        System.out.println(x.search(DNA,target3));
+
+        String[] callNumbers = {"A123", "B456", "C789", "D012", "E345"};
+        String target4 = "C789";
+        System.out.println(x.search(callNumbers,target4));
+
+        String[] serialNumbers = {"123A", "456B", "789C", "012D", "345E"};
+        String target5 = "012D";
+        System.out.println(x.search(serialNumbers,target5));
+    }
+
+}
